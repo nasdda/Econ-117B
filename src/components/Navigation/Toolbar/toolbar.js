f62c4648f122286cecb89378f3335e9f9c18064e
Xin <xin2001yan@gmail.com>
2020-07-29 19:17:33 -0700

    update toolbar

---
+++ src/components/Navigation/Toolbar/toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './toolbar.css'
+
+const toolbar = (props) => {
+    return (
+        <div className='Toolbar'>
+            <div className='Site-name'><p>Econ117B</p></div>
+            <nav className='Navigation'>
+                <p className='Navigation-option'>Home</p>
+                <p className='Navigation-option'>Quizzes</p>
+                <p className='Navigation-option'>Files</p>
+            </nav>
+
+        </div>)
+}
+
+export default toolbar
