e42f703d8847fd554864372aec4a3886f6865bc0
Jack Wange <ywang460@ucsc.edu>
2020-07-29 22:10:07 -0700

    mine

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -7,8 +7,18 @@ const home = (props) => {
         <React.Fragment>
             <Toolbar />
             <Spacer>
-                <h1>Home</h1>
-                <img src={"../../public/homework-hi.svg"}/>
+    
+                <h1 class = "welcome_message">Welcome to ECON 117B!</h1>
+              
+
+                <select name="Homework" id="homework">
+                <option value="Chapter 1">Chapter 1</option>
+                <option value="Chapter 2">Chapter 2</option>
+                <option value="Chapter 3">Chapter 3</option>
+                <option value="Chapter 4">Chapter 4</option>
+                </select>
+
+                {/* <img src={"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd"}/>  */}
                 <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
                 <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
                 <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
