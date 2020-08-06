217a571b3299a8b1e15e3a8c1b974522f7c70f30
Xin <xin2001yan@gmail.com>
2020-08-05 23:02:54 -0700

    update animation

--- src/components/Practice/practice.js
+++ src/components/Practice/practice.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState } from 'react'
 
 import Toolbar from '../Navigation/Toolbar/toolbar'
 import Chapter from './Chapters/chapter'
